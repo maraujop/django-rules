3c0f3624762657b4eb5b62b87b8ea312ed88544d
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2010-11-02 17:12:42 +0100

    Moving verbose output to stderr

--- django_rules/utils.py
+++ django_rules/utils.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from models import RulePermission
+
+import sys
+
 from django.contrib.contenttypes.models import ContentType
+
+from models import RulePermission
     
 def register(app_name, codename, model, field_name='', view_param_pk='', description=''):
     """
@@ -11,14 +15,14 @@ def register(app_name, codename, model, field_name='', view_param_pk='', descrip
     try:
         ctype = ContentType.objects.get(app_label = app_name, model = model)
     except ContentType.DoesNotExist:
-        print "! Rule codenamed %s will not be synced as model %s was not found for app %s" % (codename, model, app_name)
+        sys.stderr.write('! Rule codenamed %s will not be synced as model %s was not found for app %s\n' % (codename, model, app_name))
         return
 
     try:
         # We see if the rule's pk exists, if it does then delete and overwrite it
         rule = RulePermission.objects.get(pk = codename)
         rule.delete()
-        print "Careful rule %s being overwritten. Make sure its codename is not repeated in two rules.py files" % codename
+        sys.stderr.write('Careful rule %s being overwritten. Make sure its codename is not repeated in other rules.py files\n' % codename)
         RulePermission.objects.create(codename=codename, field_name=field_name, content_type=ctype,
                     view_param_pk=view_param_pk, description=description)
 
