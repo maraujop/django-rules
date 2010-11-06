80ae96d70c62da4da8453ffaab62bc8af8aa8453
J. Javier Maestro de la Calle <jjmaestro@ieee.org>
2010-11-07 00:14:00 +0100

    Fixing a bug in central authorization where central auth does not override rules
    Basically, central authorization was not overriding rules because rules were
    always checked and the return value was being overriten. This was also
    inefficient, because once a central auth overrides us, we should not check any
    further rule.

--- django_rules/backends.py
+++ django_rules/backends.py
@@ -25,11 +25,11 @@ class ObjectPermissionBackend(object):
         This method checks if the user_obj has perm on obj. Returns True or False
         Looks for the rule with the code_name = perm and the content_type of the obj
         If it exists returns the value of obj.field_name or obj.field_name() in case
-        the field is a method.
+        the field is a method with no arguments, or obj.field_name(user_obj) in case
+        the field is a method with arguments (the authorization constraint method can
+        only accept one instance of User).
         """
-        
-        if obj is None:
-            return False
+        isAuthorized = False
 
         if not user_obj.is_authenticated():
             user_obj = User.objects.get(pk=settings.ANONYMOUS_USER_ID)
@@ -51,49 +51,50 @@ class ObjectPermissionBackend(object):
                 raise RulesError('Error module %s does not have a central_authorization function"' % (module))
             
             try:
-                value = central_authorizations(user_obj, perm)
-                if not isinstance(value, bool):
+                isAuthorized = central_authorizations(user_obj, perm)
+                if not isinstance(isAuthorized, bool):
                     raise NotBooleanPermission("central_authorizations did not return a boolean value")
-
             except TypeError:
                 raise RulesError('central_authorizations should receive 2 parameters: (user_obj, perm)')
 
-        # Note:
-        # is_active and is_superuser are checked by default in django.contrib.auth.models
-        # lines from 301-306 in Django 1.2.3
-        # So we don't double check, If this changes, the tests will catch it
-        ctype = ContentType.objects.get_for_model(obj)
+        # If central_authorization did not override us, we must follow checking the rule
+        if not isAuthorized and obj is not None:
+            # Note:
+            # is_active and is_superuser are checked by default in django.contrib.auth.models
+            # lines from 301-306 in Django 1.2.3
+            # So we don't double check, If this changes, the tests will catch it
+            ctype = ContentType.objects.get_for_model(obj)
 
-        # We get the rule data and return the value of that rule
-        try:
-            rule = RulePermission.objects.get(codename = perm, content_type = ctype)
-        except RulePermission.DoesNotExist:
-            raise NonexistentPermission('RulePermission with codename %s on model %s does not exist',
-                                        (perm, ctype.model))
+            # We get the rule data and return the value of that rule
+            try:
+                rule = RulePermission.objects.get(codename = perm, content_type = ctype)
+            except RulePermission.DoesNotExist:
+                raise NonexistentPermission('RulePermission with codename %s on model %s does not exist',
+                                            (perm, ctype.model))
 
-        bound_field = None
-        try:
-            bound_field = getattr(obj, rule.field_name)
-        except AttributeError:
-            raise NonexistentFieldName("Field_name %s from rule %s does not longer exist in model %s. \
-                                        The rule is obsolete!", (rule.field_name, rule.codename, rule.content_type.model))
+            bound_field = None
+            try:
+                bound_field = getattr(obj, rule.field_name)
+            except AttributeError:
+                raise NonexistentFieldName("Obsolete rule (field_name %s from rule %s no longer exist in model %s)",
+                                            (rule.field_name, rule.codename, rule.content_type.model))
 
-        if not isinstance(bound_field, bool) and not callable(bound_field):
-            raise NotBooleanPermission("Attribute %s from model %s on rule %s does not return a boolean value",
-                                        (rule.field_name, rule.content_type.model, rule.codename))
+            if not isinstance(bound_field, bool) and not callable(bound_field):
+                raise NotBooleanPermission("Attribute %s from model %s on rule %s does not return a boolean value",
+                                            (rule.field_name, rule.content_type.model, rule.codename))
 
-        if not callable(bound_field):
-            return bound_field
+            if not callable(bound_field):
+                isAuthorized = bound_field
+            else:
+                # Otherwise it is a callabe bound_field
+                # Let's see if we pass or not user_obj as a parameter
+                if (len(inspect.getargspec(bound_field)[0]) == 2):
+                    isAuthorized = bound_field(user_obj)
+                else:
+                    isAuthorized = bound_field()
 
-        # Otherwise it is a callabe bound_field
-        # Let's see if we pass or not user_obj as a parameter
-        if (len(inspect.getargspec(bound_field)[0]) == 2):
-            value = bound_field(user_obj)
-        else:
-            value = bound_field()
+                if not isinstance(isAuthorized, bool):
+                    raise NotBooleanPermission("Callable %s from model %s on rule %s does not return a boolean value",
+                                                (rule.field_name, rule.content_type.model, rule.codename))
 
-        if not isinstance(value, bool):
-            raise NotBooleanPermission("Callable %s from model %s on rule %s does not return a boolean value",
-                                        (rule.field_name, rule.content_type.model, rule.codename))
-
-        return value
+        return isAuthorized
