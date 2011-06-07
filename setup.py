107d361e29d95b20f6d2729c222d8515a1396535
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2011-06-07 13:02:31 +0200

    Fixes distutils enconding issues, so that register command works

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
+reload(sys).setdefaultencoding("UTF-8")
 from setuptools import setup, find_packages
 from distutils.core import setup
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
